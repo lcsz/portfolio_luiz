8fad471cbea286f78d88a1b51b85caa2d7db1238
luizsouzajr-alt <luiz.souza@altbank.co>
2025-02-24 19:24:11 -0300

    feat: chamada de metodos do formulario

--- controllers/formularioController.js
+++ controllers/formularioController.js
@@ -4,23 +4,65 @@ class FormularioController {
 
     constructor() {
         this.botao = document.getElementById("btnEnviar");
+        this.btnDeletar = document.getElementById("btnDeletar");
+
         this.inputLogin = document.getElementById("inputLogin");
         this.inputPwd = document.getElementById("inputPwd");
         this.inputNome = document.getElementById("inputNome");
+        this.inputId = document.getElementById("inputId");
+
         this.divListaUsuario = document.getElementById("listaUsuario");
+        this.divButtonDeletar = document.getElementById("divButtonDeletar");
 
         this.login = null;
         this.pwd = null;
         this.nome = null;
+        this.usuarioId = null;
 
+        this.listaUsuarios = [];
+
+        this.changeDivButtonDeletar(false);
         this.pegarUsuarios();
+
     }
 
     listeners() {
         this.botao.addEventListener("click", this.enviar());
+        this.btnDeletar.addEventListener("click", this.deletar());
+
+
         this.inputLogin.addEventListener("keyup", this.changeLoginValue());
         this.inputPwd.addEventListener("keyup", this.changePwdValue());
         this.inputNome.addEventListener("keyup", this.changeNomeValue());
+    }
+
+
+    deletar() {
+        return (e) => {
+            let usuarioId = this.inputId.value;
+            
+            if (usuarioId != null) {
+                this.loading(true);
+                this.restHelper.delete(usuarioId).then((response) => {
+                    this.listaUsuarios = this.listaUsuarios.filter((usuario) => {
+                        return usuario.id != usuarioId;
+                    });
+                    this.limparDados();
+                    this.atualizarListaUsuario();
+                    this.loading(false);
+                });
+            }
+        }
+    }
+
+
+    changeDivButtonDeletar(mostrar) {
+        if (mostrar == true) {
+            this.divButtonDeletar.classList.remove("d-none");
+        } else {
+            this.divButtonDeletar.classList.add("d-none");
+        }
+
     }
 
     changeLoginValue() {
@@ -44,8 +86,10 @@ class FormularioController {
     loading(ligado) {
         if (ligado) {
             this.botao.disabled = true;
+            this.btnDeletar.disabled = true;
         } else {
             this.botao.disabled = false;
+            this.btnDeletar.disabled = false;
         }
     }
 
@@ -53,50 +97,104 @@ class FormularioController {
         this.inputLogin.value = "";
         this.inputPwd.value = "";
         this.inputNome.value = "";
+        this.inputId.value = "";
 
         this.login = null;
         this.pwd = null;
         this.nome = null;
+        this.usuarioId = null;
+
+        this.changeDivButtonDeletar(false);
     }
 
-    async enviarDados(data) {        
+
+    async criarDados(usuario) {
+        let novoUsuario = await this.restHelper.post(usuario);  
+        this.listaUsuarios.push(novoUsuario);              
+        return novoUsuario;
+    }
+
+
+    async atualizarDados(usuario) {        
+        let usuarioAtualizado = await this.restHelper.put(usuario.id, {
+            "name": usuario.name,
+            "email": usuario.email,
+            "password": usuario.password
+        });
+
+        this.listaUsuarios = this.listaUsuarios.map((usuario) => {
+            if (usuario.id == usuarioAtualizado.id) {
+                return usuarioAtualizado;
+            }
+            return usuario;
+        });
+
+        return usuarioAtualizado;
+    }
+
+
+    async enviarDados(data) {     
         this.loading(true);
-        let usuario = await this.restHelper.post(data);
+        
+        if (data.id == null) {
+            let usuario = await this.criarDados(data);
+            this.atualizarListaUsuario();
+        } else {
+            let usuario = await this.atualizarDados(data);
+            this.atualizarListaUsuario();
+        }
+
         this.limparDados();
         this.loading(false);
-        this.atualizarListaUsuario(usuario);
+        
     }
 
-    atualizarListaUsuario(usuario) {
-        let novoUsuario = `<li class="list-group-item" onclick="alert(${usuario.id})">${usuario.id} - ${usuario.name} - ${usuario.email} - ******</li>`;
-        this.divListaUsuario.innerHTML += novoUsuario;
-    }
+    atualizarListaUsuario() {
+        this.divListaUsuario.innerHTML = "";
 
-    teste(id) {
-        return (e) => {
-            alert(id)
-        }
+        this.listaUsuarios.forEach((usuario) => {
+            let novoUsuario = `<li id="usuario-${usuario.id}" class="list-group-item" onclick="atualizar(${usuario.id}, '${usuario.name}', '${usuario.email}', '${usuario.pwd}')">${usuario.id} - ${usuario.name} - ${usuario.email} - ******</li>`;
+            this.divListaUsuario.innerHTML += novoUsuario;
+        })
     }
 
     enviar() {
-        return (e) => {                
+        return (e) => {             
+            
+            let idValue = this.inputId.value;            
             this.enviarDados({
+                "id": idValue == "" ? null : idValue,
                 "name": this.nome,
                 "email": this.login,
                 "password": this.pwd
-            });            
+            });                    
+            
         }
     }
 
-    pegarUsuarios() {
+    pegarUsuarios() {        
         this.restHelper.get().then((usuarios) => {
-            usuarios.forEach(usuario => {
-                this.atualizarListaUsuario(usuario);
-            });
+            this.listaUsuarios = usuarios;            
+            this.atualizarListaUsuario();
+            
         });
+    }
+
+    colocarDadosFormulario(id, nome, email, pwd) {
+        
+        this.inputLogin.value = email;
+        this.inputPwd.value = pwd;
+        this.inputNome.value = nome;
+        this.inputId.value = id;
+
+        this.login = email;
+        this.pwd = pwd;
+        this.nome = nome;
+        this.usuarioId = id;
+
+        this.changeDivButtonDeletar(true);
     }
 
 }
 
 
-new FormularioController().listeners();
